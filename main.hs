f097b33305688845a871c639a9e0b7ba50907b97
sSalvi <jaosalvi@protonmail.com>
2022-11-12 16:16:01 -0300

    Update main.hs

--- main.hs
+++ main.hs
@@ -1,16 +1,22 @@
-data Group = Empty | Group {
+data Group = Group {
     idf :: Int,
     name :: String,
-    lat :: Int,
-    long :: Int,
+    point :: (Int, Int),
     nw :: Maybe Group,
     ne :: Maybe Group,
     sw :: Maybe Group,
     se :: Maybe Group
 } deriving Show
 
+-- Aux func
+initG idf name (lat, long) = Group idf name (lat, long) Nothing Nothing Nothing Nothing
+getLat (Group _ _ p _ _ _ _) = fst p
+getLong (Group _ _ p _ _ _ _) = snd p
 
 main = do
-    -- let g1 = Group { idf = 0, name = "Ababa", lat = 32, long = 40, nw = Nothing, ne = Nothing, sw = Nothing, se = Nothing}
-    -- let g1 = Group { idf = 0, name = "Ababa", lat = 32, long = 40, nw = Empty, ne = Empty, sw = Empty, se = Empty}
-    print g1
+    let gList = []
+    let g1 = initG 1 "Louisville" (38, 85)
+    let g2 = initG 2 "Chicago" (41, 87)
+    let gList = g1 : g2 : []
+    let hd = head gList
+    print $ name hd
