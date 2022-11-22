c6e75a9c15d682648ecf669d7a763336f1c75eb9
sSalvi <jaosalvi@protonmail.com>
2022-11-22 18:36:29 -0300

    Update main.hs

--- main.hs
+++ main.hs
@@ -1,8 +1,62 @@
-data Tree = Leaf Int String Int Int
-    | Node Tree Tree Tree Tree
+import Data.String
+
+data Tree = 
+      Leaf {
+        name :: String, 
+        lat :: Integer,
+        long :: Integer}
+    | Node { 
+        name :: String  ,
+        lat :: Integer, 
+        long:: Integer,
+        nw :: Tree,
+        ne :: Tree,
+        sw :: Tree,
+        se :: Tree}
+    | Empty
     deriving Show
 
-height :: Tree -> Int
-height (Leaf _ _ _ _) = 1
-height (Node nw ne sw se) = 1 + (max (max (height nw) (height ne)) (max (height sw) (height se)))
+main = do
+    let maintree = Node{
+                        name  = "Louisville",
+                        lat = 38, 
+                        long = 85,
+                        nw = Leaf {         --NW RAIZ
+                            name = "Chicago",
+                            lat = 41,
+                            long = 87},
+                        ne = Node                   --NE RAIZ
+                        {
+                            name = "Washington",
+                            lat = 38,
+                            long = 77,
+                            nw = Node                   --NW Wash
+                            {
+                                name = "Pittsburgh",
+                                lat = 40, 
+                                long = 79,
+                                nw = Leaf {name = "Cleavland", lat = 41, long = 81}, --NW Pitt
+                                ne = Empty,                   --NE Pitt
+                                sw = Leaf{ name = "Dayton", lat = 39, long = 84},    --SW Pitt
+                                se = Empty                   --SE Pitt
+                            },
+                            ne = Node                  --NE Wash 
+                            {
+                                name = "New York", 
+                                lat = 40,
+                                long = 74,
+                                nw = Empty,                   --NW NY
+                                ne = Leaf{name = "Montreal", lat = 45, long = 73},  --NE NY
+                                sw = Empty,                   --SW NY
+                                se = Empty                   --SE NY
+                            },
+                            sw = Empty,                   --SW Wash 
+                            se = Empty                   --SE Wash 
+                        },                  
+                        sw = Leaf{name = "Nashville", lat = 36, long = 38}, --SW RAIZ
+                        se = Leaf{name = "Atlanta", lat = 34, long = 84}    --SE RAIZ
+                    }
+    print maintree
+
+
 
